affbdee81108582777441cff2c9096444db0303b
ArnaultNouvel <ArnaultNouvel@users.noreply.github.com>
2021-11-04 07:54:20 +0000

    deploy: 946f53569b0b985e9b6ef68d8ec64477aac1d439

--- _next/static/5_p2PcEIONn2MkCsxPcSX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-ee9594b91b258940.js","static/chunks/987-935b9bc933906350.js","static/chunks/pages/index-3e7830010e29439f.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/dMzVNccbx6zSPajrubB0N/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-ee9594b91b258940.js","static/chunks/987-935b9bc933906350.js","static/chunks/pages/index-306aaff2020134db.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
