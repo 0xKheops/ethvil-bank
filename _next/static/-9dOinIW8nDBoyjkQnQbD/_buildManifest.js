1aaaade6ed78b6e6072b84cd5388ad22b53fa1a6
ArnaultNouvel <ArnaultNouvel@users.noreply.github.com>
2021-11-04 01:25:16 +0000

    deploy: d130975ea80f41615f69aa823cb7d411a212f882

--- _next/static/-9dOinIW8nDBoyjkQnQbD/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-ee9594b91b258940.js","static/chunks/245-d2600a8773a4cb32.js","static/chunks/pages/index-3b2129a475a45caf.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
